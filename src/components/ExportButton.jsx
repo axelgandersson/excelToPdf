bb42c65602e26a2fe8c44dbc8528de523d85e42c
Rebecka Johansson <rebeckaemmajohansson@gmail.com>
2025-09-24 15:43:20 +0200

    styling till exportbutton
    installerat react-icons med npm, stylat knappen i ExportButton.jsx

--- src/components/ExportButton.jsx
+++ src/components/ExportButton.jsx
@@ -1,5 +1,16 @@
+import { FaFileExport } from "react-icons/fa";
+
 function ExportButton() {
-  return <div>exportera knapp</div>;
+  return (
+    <div>
+      <button className="flex items-center gap-3 py-3 px-5 bg-slate-900 text-white cursor-pointer rounded-3xl hover:bg-slate-700 transition font-semibold border-l-8 border-slate-700">
+        Export to PDF
+        <span>
+          <FaFileExport />
+        </span>
+      </button>
+    </div>
+  );
 }
 
 export default ExportButton;
