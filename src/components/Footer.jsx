3ffef412ccdf851b919f01aa014aaf8ad189642f
TomasTolo <tolotomas04@outlook.com>
2025-09-27 02:03:49 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ function Footer() {
   return (
     <footer className="bg-slate-900/60 border-t border-slate-700 p-4 w-full">
       <div className="container mx-auto flex justify-between items-center text-sm">
-        <p className="text-slate-500">ExcelWebManager.</p>
         <div className="flex space-x-4">
           
         </div>
