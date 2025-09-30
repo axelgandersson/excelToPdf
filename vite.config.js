884353fe3115e061ab2e06e54cea5db13e68b22e
Lisette <Lisettestorm@hotmail.com>
2025-09-30 12:33:40 +0200

    Lägga upp GH-pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    optimizeDeps: {
-        include: ["jspdf", "jspdf-autotable"],
-    },
+  plugins: [react(), tailwindcss()],
+  base: "/excelToPdf/",
+  optimizeDeps: {
+    include: ["jspdf", "jspdf-autotable"],
+  },
 });
